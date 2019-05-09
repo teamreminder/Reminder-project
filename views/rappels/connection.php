ac51e8458a8ef7c89357da2cd45d97b3b6d353d9
Florent <florent19du86@gmail.com>
2019-05-09 09:51:50 +0200

    finition COOKIE

--- views/rappels/connection.php
+++ views/rappels/connection.php
@@ -1,16 +1,9 @@
 <?php
-// $list=[];
+
 if ((isset($_GET['email']) && !empty($_GET['email']) && (strlen($_GET['email']) <= 300))&&(isset($_GET['password']) && !empty($_GET['password']) && (strlen($_GET['password']) <= 300)))
  {
-   if (isset($list)) {
-     setcookie('utilisateur',$list['id_user'],time()+6000);
-     var_dump($list);
-  //   header("location: ?controller=rappels&action=home");
+   header("location: ?controller=rappels&action=home");
 
-   }else{
-     echo "Erreur d'identification";
-     echo "<br><a href='index.php'>retour page de connection</a>";
-   }
  }else {
    echo "formulaire mal rempli !";
    echo "<br><a href='index.php'>retour page de connection</a>";
