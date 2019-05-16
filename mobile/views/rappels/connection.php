5c7a4d9bb5c25f5b23c290b69162ec02ad1ca4fa
Florent <florent19du86@gmail.com>
2019-05-16 10:21:17 +0200

    xsd

--- mobile/views/rappels/connection.php
+++ mobile/views/rappels/connection.php
@@ -3,10 +3,10 @@ foreach ($posts as $post) {
 if ((isset($_GET['email']) && !empty($_GET['email']) && (strlen($_GET['email']) <= 300))&&(isset($_GET['password']) && !empty($_GET['password']) && (strlen($_GET['password']) <= 300)))
  {
    if ($_GET['email']==$post->email && hash('sha512', $_GET['password'])==$post->password) {
-     $id=$post->id;
+   $id=$post->id;
    echo "cookie ok";
    setcookie('utilisateur',$id,time()+6000);
-   header("location: ?controller=rappels&action=home");
+   // header("location: ?controller=rappels&action=home");
    }else{
       echo "erreur d'identification";
  }
