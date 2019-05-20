fbb1b948a1a2a1ec2d6e95c15bd81ed3db2c6bcb
Delpech <Charlesdelpech1@gmail.com>
2019-05-20 10:08:10 +0200

    qershq

--- mobile/views/rappels/home.php
+++ mobile/views/rappels/home.php
@@ -19,7 +19,7 @@
         ?>
     <div class="row">
       <div class="col-8">
-        <p><?php echo $post->destinataire; ?><br>
+        <p><?php echo $post->email; ?><br>
          <?php echo "Objet : ".$post->objet."<br>";
          echo substr($post->date_rappel, 0, -9);
           ?></p>
@@ -33,29 +33,6 @@
     <?php
    }
    ?>
-  </div>
-</div>
-<div class="container">
-   <div class="list_reminder">
-     <?php
-       foreach ($posts as $post) {
-         ?>
-     <div class="row">
-       <div class="col-8">
-         <p><?php echo $post->email; ?><br>
-          <?php echo "Objet : ".$post->objet."<br>";
-          echo substr($post->date_rappel, 0, -9);
-           ?></p>
-       </div>
-       <div class="col-4 align-self-end">
-         <a href='edit_remind.php'><img src="img/pens.png"=""></a>
-         <a href='#'><img src="img/cross.png"=""></a>
-       </div>
-     </div>
-     <hr>
-     <?php   
-    }
-    ?>
    </div>
  </div>
 <div class="ad">
