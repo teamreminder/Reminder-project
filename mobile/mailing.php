c021b07864167c927547a86f935539288c60980f
Delpech <Charlesdelpech1@gmail.com>
2019-05-22 15:02:33 +0200

    gsfgnsgfn

--- mobile/mailing.php
+++ mobile/mailing.php
@@ -3,8 +3,8 @@
 $bdd = new PDO('mysql:host=remindmeuiremind.mysql.db;dbname=remindmeuiremind;charset=utf8', 'remindmeuiremind', 'Isfac2019', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 // $bdd = new PDO('mysql:host=localhost;dbname=reminder', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
-$requete = "SELECT date_rappel, email, objet, message
-            FROM rappel";
+$requete = "SELECT id_rappel,date_rappel,message, objet, email, rappel.id_user as id_destinataire
+            FROM rappel INNER JOIN user ON user.id_user = rappel.id_user_etre_destinataire";
 $result=$bdd->query($requete);
 $resultat=$result->fetchAll();
 foreach ($resultat as $value)
@@ -12,7 +12,9 @@ foreach ($resultat as $value)
   $date_rappel=$value['date_rappel'];
   $email=$value['email'];
   $objet=$value['objet'];
-  $message=$value['message'];
+  $content=$value['message'];
+  echo date('Y-m-d H')."<br>";
+  echo substr($date_rappel, 0, -6)."<br>";
 
   if (date('Y-m-d H')==substr($date_rappel, 0, -6)) {
     $header="MIME-Version: 1.0\r\n";
@@ -20,18 +22,18 @@ foreach ($resultat as $value)
     $header.='Content-Type:text/html; charset="uft-8"'."\n";
     $header.='Content-Transfer-Encoding: 8bit';
 
-    $content='
+    $message='
     <html>
       <body>
         <div align="center">
           <h1>Reminder</h1>
         </div>
-        '.$message.'
+        '.$content.'
       </body>
     </html>
     ';
 
-    mail($email,$objet,$content,$header);
+    mail($email,$objet,$message,$header);
   }
 }
  ?>
