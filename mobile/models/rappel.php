d4bc7a1c1e493946ac51daae4689823fa73a9dfc
Florent <florent19du86@gmail.com>
2019-05-22 13:47:12 +0200

    sfzf

--- mobile/models/rappel.php
+++ mobile/models/rappel.php
@@ -1,42 +1,49 @@
 <?php
 class Rappel {
-  public $id_rappel;
-  public $date_rappel;
-  public $objet;
-  public $email;
   public $id_user;
+  public $objet;
+  public $date_rappel;
+  public $id_rappel;
+  public $id_user_etre_destinataire;
+  public $email;
 
 
-  public function __construct($id_rappel, $date_rappel, $objet, $email, $id_user) {
-    $this->id_rappel = $id_rappel;
-    $this->date_rappel = $date_rappel;
-    $this->objet = $objet;
-    $this->email = $email;
+  public function __construct($id_user, $objet, $date_rappel, $id_rappel, $id_user_etre_destinataire, $email) {
     $this->id_user = $id_user;
+    $this->objet = $objet;
+    $this->date_rappel = $date_rappel;
+    $this->id_rappel = $id_rappel;
+    $this->id_user_etre_destinataire = $id_user_etre_destinataire;
+    $this->email = $email;
     }
+
+  public static function createReminder() {
+    $db=Db::getInstance();
+    $id=$_COOKIE['utilisateur'];
+    $req = $db->query("SELECT id_user, objet, date_rappel, id_rappel, id_user_etre_destinataire FROM `rappel` WHERE id_user=$id");
+      foreach($req->fetchAll() as $post) {
+        $list[] = new User($post['id_user'], $post['objet'], $post['date_rappel'], $post['id_rappel'], $post['id_user_etre_destinataire']);
+        $destinataire=$post['id_user_etre_destinataire'];
+        $req = $db->query("SELECT email FROM `user` WHERE id_user=$destinataire");
+        foreach($req->fetchAll() as $post) {
+          $tableau[] = new User($post['email'], $post['email'], $post['email'], $post['email'], $post['email'], $post['email']);
+        }
+      return $tableau;
+      }
+    return $list;
+  }
 
   public static function createReminderTraitement() {
     $db=Db::getInstance();
-    $email=$_GET['destinataire'];
+    $destinataire=$_GET['destinataire'];
     $datetime=$_GET['datetime'];
     $objet=$_GET['objet'];
     $message=$_GET['message'];
     $cookie=$_COOKIE['utilisateur'];
     $today=date("Y-m-d H:i:s");
-    $id_destinataire="";
 
-    $requete1="SELECT id_user,email FROM user WHERE email='$email'";
-    $reponse=$db->query($requete1);
 
-    foreach ($reponse as $info)
-    {
-     $id_destinataire = $info['id_user'];
-     echo $info;
-    }
-
-    $req2="INSERT INTO rappel (objet, date_rappel, message, date_enregistrement, id_user, id_user_etre_destinataire)
-    VALUES ( $objet, $datetime, $message, $today, $cookie, $id_destinataire)";
-    $db->query($req2);
+    $req="INSERT INTO `rappel` (`objet`, `date_rappel`, `message`, `date_enregistrement`, `id_user`, `id_user_etre_destinataire`) VALUES ($objet, $datetime, $message, $today, $cookie, '3')";
   }
 
   public static function home() {
@@ -45,14 +52,18 @@ class Rappel {
     $cookie=$_COOKIE['utilisateur'];
     $req = $db->query("SELECT id_rappel, date_rappel, objet, email, rappel.id_user as id_destinataire
                        FROM rappel INNER JOIN user ON user.id_user = rappel.id_user_etre_destinataire
-                       WHERE rappel.id_user = $cookie");
+                       WHERE rappel.id_user = 2");
+    // $reponse = $db->query($req);
+    // foreach ($reponse as $info) {
+    //   $slots=$info['slots'];
+
     foreach($req->fetchAll() as $post) {
-      $list[] = new Rappel($post['id_rappel'], $post['date_rappel'], $post['objet'], $post['email'], $post['id_destinataire']);
+      $list[] = new Rappel($post['id_rappel'], $post['date_rappel'], $post['objet'], $post['email'], $post['id_user'], $post['slots']);
     }
     return $list;
   }
 
-}
+  }
 
   // public static function createArdoise($prenom,$montant){
   //   $db = Db::getInstance();
