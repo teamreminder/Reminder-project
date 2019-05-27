57d88c8c79b0978e1e8cde4cd1a18b248a41cc22
Florent <florent19du86@gmail.com>
2019-05-27 09:10:35 +0200

    ezg

--- mobile/views/rappels/create_contact_traitement.php
+++ mobile/views/rappels/create_contact_traitement.php
@@ -12,16 +12,16 @@ $nom=$_GET['nom'];
     if( isset($_GET['email']) && !empty($_GET['email']) && (strlen($_GET['email']) <= 300) &&
         isset($_GET['prenom']) && !empty($_GET['prenom']) && (strlen($_GET['prenom']) <=50) &&
         isset($_GET['nom']) && !empty($_GET['nom']) && (strlen($_GET['nom']) <=50)) {
-          ?>
-            <p>Le contact a bien été créé.</p>
-          <?php
+
+            echo "<p>Le contact a bien été créé</p>";
+
         }else{
-          ?>
-            <p>Erreur dans le remplissage du formulaire.</p>
-          <?php
+
+            echo "<p>Erreur dans le remplissage du formulaire.</p>";
+
         }
         ?>
-    <a href='?controller=rappels&action=home'>retour à l'accueil</a>
+    <a href='?controller=rappels&action=GestionContact'>retour à l'accueil</a>
   </div>
 </div>
 <?php
