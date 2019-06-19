a22a8996b425f3c2ebdb7270a72bb7a6c00dd608
Florent <florent19du86@gmail.com>
2019-06-19 09:43:21 +0200

    sdf

--- mobile/models/rappel.php
+++ mobile/models/rappel.php
@@ -117,7 +117,17 @@ class Rappel {
               <h1>Reminder</h1>
             </div>
             <p>Bonjour Mme/M.<br><br>Notre utilisateur $expediteur souhaite vous envoyez un mail de rappel grâce à notre application Reminder<br><br>Pour accepter cette invitation, inscrivez-vous! Cela ne prendra qu'un instant</p>
-            <br><br><a href='http://remind-me.fr/index.php?controller=rappels&action=registerByMail&id=$id_user_destinataire'>S'inscrire</a>
+            <br><br><a href='http://remind-me.fr/index.php?controller=rappels&action=registerByMail&id=$id_user_destinataire'>J'accepte</a>
+            <a href='http://remind-me.fr/index.php?controller=rappels&action=registerByMail&id=$id_user_destinataire'>Je refuse</a>
+            <style>
+            a {
+               border: 1px solid #DB9000;
+               padding: 10px;
+               border-radius: 3px;
+               color: #DB9000;
+               text-decoration: none;
+              }
+            </style>
           </body>
         </html>
         ";
